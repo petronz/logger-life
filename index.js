b5335a471f9c33b09ba0a6301cc87f41143b238d
Luca Petri <petronz90@gmail.com>
2017-05-03 16:41:39 +0200

    doc

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 const Logger = require("./Logger.class.js");
 
 var logger = new Logger({
-  formatColors: {
-    warn: "reverse.fg_magenta.bg_yellow"
-  },
-  logFunction: console.log,
+  /*
+  * [String]level: available levels are debug, info, warn and error.
+  * They accepts a string of available color concatenations (with "." as separator)
+  */
+  level: "debug",
+  /*
+  * [String]formatText: the console logged message.
+  * you can customize it with the interpolation of the 3 major variables:
+  * %level, %date and %content.
+  * You can also use customInterpolations like in the example %customDate
+  */
   formatText: "%level %customDate - %content ",
+  /*
+  * [Object]formatActions: it accepts an object populated with (non-void)functions.
+  * It is used to add custom actions that can be used into formatText
+  */
   formatActions: {
     customDate: (options) => {
       return options.date.toISOString();
     }
   },
+  /*
+  * [Object]formatColors: available keys of formatColors are:
+  * debug, info, warn and error.
+  * They accepts a string of available color concatenations (with "." as separator)
+  */
+  formatColors: {
+    warn: "reverse.fg_magenta.bg_white",
+    error: "bg_white.fg_red"
+  },
+  /*
+  * [Function]logFunction: accept an arbitrary function.
+  * it is used to log on console. Default: console.log
+  */
+  logFunction: console.log,
+  /*
+  * [Object]labels: available keys of labels are:
+  * debug, info, warn and error.
+  * They accepts an arbitrary string to print on console as level when %level into formatText is requested
+  */
   labels: {
     warn: "[WARNING] "
   },
+  /*
+  * [Object]fileLog: available keys of fileLog are:
+  * path and level.
+  * path is where has to be saved the log,
+  * level contains all the 4 levels warn, error, info and debug; for each one of them
+  * you spcify if you want to log on file or not
+  */
   fileLog: {
     path: __dirname,
     level: {
@@ -25,15 +62,26 @@ var logger = new Logger({
   }
 });
 
+/*
+* addRankAction([String]rank, [Function]callback):
+* specify for a certain rank a callback to execute every time the specific rank is logged
+*/
 logger.addRankAction("debug", (data) => {
-  console.log("PROVIDED A DEBUG");
-})
+  console.log(`Provided a debug log, it has ${data.content.formatted}`);
+});
 
-logger.log("debug", "ciao [[reverse:sono]] il debug brutto, perchè nessuno mi vuole");
+/*
+* addLevelAction([String]level, [Function]callback):
+* specify for a certain level a callback to execute every time the level is logged
+*/
+logger.addLevelAction("info", (data) => {
+  console.log(`Provided a ${data.level} log, it has ${data.content.formatted}`);
+});
 
-logger.log("info", "ciao info il piu bello di [[blink.underscore.fg_red:tutti]] perchè di solito tutti possono leggermi");
-logger.log("info", "ciao info il piu bello di [[reverse:tutti]] perchè di solito tutti possono leggermi");
+logger.log("debug", "Hello! I'm [[underscore:Debug]] and i am very useful");
 
-logger.log("warn", "ciao sono il warn bello, ma oggi potrebbe essere una brutta giornata");
+logger.info("Hello! I'm [[reverse.bg_yellow:Info]] and i am [[underscore:very useful]]");
 
-logger.log("error", "ciao sono l'errore bello");
+logger.log("warn", "Hello! I'm [[reverse.bg_yellow:warn]] [[and i am very useful]]");
+
+logger.error("Hello! I'm [[reverse.bg_yellow:error]] and i am [[underscore.reverse.dim:very useful]]");
